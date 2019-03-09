9d015ea973130d23e5fbdca8e55ddfc7033b6239
Tomas Nilsson <tomas.nilsson92@gmail.com>
2019-03-09 19:30:48 +0100

    Add full-screen header with title and background image

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+const Header = ({ onClick }) => (
+  <div class="main-slider">
+    <div class="slider-content">
+      <h3 class="pre-title">
+        <span class="custom-color">W</span>edding
+      </h3>
+      <h1 class="title">
+        Alice <i class="icon icon-heart" /> Bob
+      </h1>
+      <h5 class="date">5 October 2030</h5>
+    </div>
+  </div>
+)
+
+export default Header
