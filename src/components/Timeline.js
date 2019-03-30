434199f0146adc831a4cb04592128e4467377872
Tomas Nilsson <tomas.nilsson92@gmail.com>
2019-03-31 00:37:20 +0100

    Make timeline more compact

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -4,16 +4,34 @@ import './Timeline.css'
 
 const Timeline = ({ items }) => (
   <div className="timeline">
-    {items &&
-      items.map((item, i) => (
-        <TimelineItem
-          key={item.date}
-          date={item.date}
-          text={item.text}
-          image={item.image}
-          textRight={i % 2 === 1}
-        />
-      ))}
+    <div className="timeline-left col-xs-12 col-md-6">
+      {items &&
+        items
+          .filter(item => item.side === 'left')
+          .map((item, i) => (
+            <TimelineItem
+              key={item.date}
+              date={item.date}
+              text={item.text}
+              image={item.image}
+            />
+          ))}
+    </div>
+    <div className="timeline-right col-xs-12 col-md-6">
+      <div className="timeline-spacing" />
+      {items &&
+        items
+          .filter(item => item.side === 'right')
+          .map((item, i) => (
+            <TimelineItem
+              key={item.date}
+              date={item.date}
+              text={item.text}
+              image={item.image}
+              rightSide
+            />
+          ))}
+    </div>
   </div>
 )
 
--- src/components/TimelineItem.js
+++ src/components/TimelineItem.js
@@ -1,25 +1,19 @@
 import React from 'react'
 import './TimelineItem.css'
 
-const TimelineItem = ({ date, text, image, textRight }) =>
-  textRight ? (
-    <div className="timeline-item">
-      <div className="timeline-thumbnail-left story-img" />
-      <div className="timeline-right-side col-xs-12 col-md-6 date-circle">
-        <div className="timeline-arrow" />
-        <h4 className="timeline-title">{date}</h4>
-        <p>{text}</p>
-      </div>
+const TimelineItem = ({ date, text, image, rightSide }) => (
+  <div className="timeline-item">
+    <div
+      className={`${
+        rightSide ? 'timeline-right-side' : 'timeline-left-side'
+      } date-circle`}
+    >
+      <div className="timeline-arrow" />
+      <h4 className="timeline-title">{date}</h4>
+      <p>{text}</p>
+      {image && <img src={image} alt={date} className="timeline-image" />}
     </div>
-  ) : (
-    <div className="timeline-item">
-      <div className="timeline-left-side col-xs-12 col-md-6 date-circle">
-        <div className="timeline-arrow" />
-        <h4 className="timeline-title">{date}</h4>
-        <p>{text}</p>
-      </div>
-      <div className="timeline-thumbnail-right story-img" />
-    </div>
-  )
+  </div>
+)
 
 export default TimelineItem
