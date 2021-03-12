ef9f2ba52d511cd3392c5e746d654a1a011f5681
Tomas Nilsson <tomas.nilsson92@gmail.com>
2021-03-12 22:03:31 +0100

    Add tooltip support for MapMarker

--- src/components/MapMarker/MapMarker.js
+++ src/components/MapMarker/MapMarker.js
@@ -1,45 +1,18 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
+import classNames from 'classnames'
+import styles from './MapMarker.module.css'
 
-const MARKER_SIZE = 40
-
-const styles = {
-  marker: {
-    position: 'absolute',
-    width: MARKER_SIZE,
-    height: MARKER_SIZE,
-    left: -MARKER_SIZE / 2,
-    top: -MARKER_SIZE / 2,
-    border: '5px solid #000000',
-    borderRadius: MARKER_SIZE,
-    backgroundColor: '#ffffff',
-    textAlign: 'center',
-    color: '#890f3a',
-    fontSize: 35,
-    padding: 4,
-    cursor: 'pointer',
-  },
-  hover: {
-    border: '5px solid #890f3a',
-    color: '#000000',
-  },
-}
-
-class MapMarker extends Component {
-  render() {
-    const { $hover, icon } = this.props
-    return (
-      <div style={{ ...styles.marker, ...($hover ? styles.hover : {}) }}>
-        <i className={`icon icon-${icon}`} />
+const MapMarker = ({ $hover, icon, title, showTooltip }) => (
+  // google-map-react pass $hover prop to hovered component
+  <div className={classNames(styles.marker, $hover && styles.markerHover)}>
+    <i className={`icon icon-${icon}`} />
+    {$hover && showTooltip && title && (
+      <div className={styles.tooltip}>
+        <div className={styles.tooltipLabel}>{title}</div>
+        <div className={styles.tooltipArrow} />
       </div>
-    )
-  }
-}
-
-MapMarker.propTypes = {
-  // GoogleMap pass $hover props to hovered components.
-  $hover: PropTypes.bool,
-  icon: PropTypes.string.isRequired,
-}
+    )}
+  </div>
+)
 
 export default MapMarker
